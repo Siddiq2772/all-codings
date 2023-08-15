a5c59b018a42a273555dfe7cc3e40a35914337fa
siddiq2772 <siddiqmohd2772@gmail.com>
2023-08-15 22:38:52 +0530

    sorting

--- binarysearch.c
+++ binarysearch.c
@@ -1,10 +1,40 @@
 #include<stdio.h>
 #include<conio.h>
-void binary_search(int num);
+void array();
 void main()
 {
-    int num;
-    printf("enter number:\n");
-    scanf("%d",&num);
+    int num,i;
+    array();
+    
+    
+    getch();
 
+}
+void array()
+{
+    int num[10],i,j,temp;
+    printf("enter 10 numbers:");
+    for(i=0;i<10;i++)
+    scanf("%d",&num[i]);
+    for(i=0;i<10;i++)
+    {
+        for(j=0;j<10;j++){
+            while(num[i]>num[i+1])
+            {temp=num[i];
+            num[i]=num[i+1];
+            num[i+1]=temp; }   
+        }
+    }
+    
+    for(i=0;i<10;i++)
+    {
+    printf("%d",num[i]);
+    }
+
+}
+void binary_search(int num)
+{
+    int i;
+    
+    
 }
