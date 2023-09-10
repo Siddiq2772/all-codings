81fa4c1155b8b50515c9090616fb42337f1aa33d
siddiq2772 <siddiqmohd2772@gmail.com>
2023-09-10 17:58:56 +0530

    complete binary search with bubble sort

--- binarysearch.c
+++ binarysearch.c
@@ -1,47 +1,73 @@
+//complete binary search with bubble sort   
 #include<stdio.h>
 #include<conio.h>
+int num[10];
 void sort();
-void main()
+ void binary_search(int target);
+int main()
 {
     int num,i;
+   
    sort();
-    printf("%d",&num)
+    printf("enter number to search: ");
+    scanf("%d",&num);
+    binary_search(num);
+   
     
 
-
+return 0;
 }
 void sort()
 {
-    int num[10],i,j,temp;
+    int i,j,temp;
     printf("enter 10 numbers:");
     for(i=0;i<10;i++)
     scanf("%d",&num[i]);
     for(i=0;i<10;i++)
     {
-        for(j=0;j=(9-i);j++){
+        for(j=0;j<(9-i);j++){
         
-            while(num[i]>num[i+1])
+            while(num[j]>num[j+1])
             {
-            temp=num[i];
-            num[i]=num[i+1];
-            num[i+1]=temp; 
+            temp=num[j];
+            num[j]=num[j+1];
+            num[j+1]=temp; 
             }   
         }
     }
     
 
 }
-void binary_search(int num[10])
+void binary_search(int target)
 {
     int i;
   int  low=0, high=9,mid;
   mid=(high+low)/2;
-  while (num[mid]==num)
+  if(target==num[mid])
   {
+  printf("%d is found at %d location",target,mid);
+ 
+  }
+  else{
+  while (num[mid]!=target)
+  {
+    for(i=0;i<10;i++)
+    {
+  if(num[i]>target)
+      high=mid-1;
+  else
+   low=mid+1;
+    mid=(high+low)/2;
+
+  }
   
-  if()
-
-
-    
-    
+  
+  }
+  if(target==num[mid])
+  printf("%d is found at %d location",target,mid);
+  else
+  printf("target not found");
 }
+
+    
+}   
